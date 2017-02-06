47e31ce71f01fb3630318199d310fa2ac15f895f
Sasha <sashanettm@gmail.com>
2017-02-06 10:01:03 +0200

    second commit

---
+++ src/main/java/dwfe/AAAA.java
@@ -0,0 +1,22 @@
+package dwfe;
+
+
+public class AAAA {
+
+
+    private String line;
+
+    public AAAA() {
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+}
+
+
+
